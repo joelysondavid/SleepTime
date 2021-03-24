9230e1deed75cebe82de09c1f65603b4ccc70e52
joedav <joelyson.david@live.com>
2021-03-23 22:54:43 -0300

    Adicionando informações

--- lib/views/sleep_time_info.dart
+++ lib/views/sleep_time_info.dart
@@ -3,18 +3,50 @@ import 'package:flutter/material.dart';
 import '../utils/app_colors.dart';
 
 class SleepTimeInfo extends StatelessWidget {
+  var appBar = AppBar(
+    title: Text("Informações"),
+    backgroundColor: AppColors.BACKGROUND_COLOR,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Informações"),
-        backgroundColor: AppColors.BACKGROUND_COLOR,
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Informações"),
+      appBar: appBar,
+      backgroundColor: AppColors.BACKGROUND_COLOR,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      "O cálculo do tempo de sono deve ser feito a partir do momento que se adormece e não no momento em que se deita, pois nem sempre a hora de deitar corresponde à hora que se adormece. Por isso, antes de fazer o cálculo, é importante acrescentar o tempo que normalmente se leva para dormir, o que é em média 15 a 30 minutos.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      "A quantidade de ciclos de 90 minutos que se dorme é variável e depende da necessidade de cada pessoa, porém o segredo é permitir que cada ciclo se complete totalmente, acordando apenas no final deste.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        color: AppColors.BACKGROUND_COLOR,
       ),
     );
   }
